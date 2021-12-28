54160f4601bc015aa453b80fa85eb447d892f437
hyeonic <dev.hyeonic@gmail.com>
2021-12-28 21:11:09 +0900

    feat: Add CenterImage in components (#3)
    이미지 중앙 보기를 위한 컴포넌트 추가

---
+++ docs/.vuepress/components/CenterImage.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div>
+        <p align=center>
+            <img :src=imageSrc />
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        imageSrc: String
+    }
+}
+</script>

e181e9905a763299516caf00775e23c8bece5423
hyeonic <dev.hyeonic@gmail.com>
2022-02-06 22:53:23 +0900

    feat: Add Comment config

---
+++ docs/.vuepress/components/Comment.vue
@@ -0,0 +1,20 @@
+<template>
+  <div ref="comment"></div>
+</template>
+<script>
+export default {
+  mounted() {
+    const utterances = document.createElement('script');
+    utterances.type = 'text/javascript';
+    utterances.async = true;
+    utterances.crossorigin = 'anonymous';
+    utterances.src = 'https://utteranc.es/client.js';
+    
+    utterances.setAttribute('issue-term', 'pathname');
+    utterances.setAttribute('theme','github-light');
+    utterances.setAttribute('repo',`hyeonic/blog-comment`);
+
+    this.$refs.comment.appendChild(utterances);
+  }
+}
+</script>
---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <ParentLayout >
+    <Comment slot="page-bottom"/>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+import Comment from '../components/Comment.vue'
+export default {
+  components: {
+    ParentLayout,
+    Comment
+  }
+}
+</script>

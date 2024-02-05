5290abdb5b4e0750f4b5a20c96a37fa643c1deb5
hyeonic <dev.hyeonic@gmail.com>
2024-02-05 23:09:42 +0900

    feat: hoodie 템플릿 추가

---
+++ blog-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: "나누면 배가 되고",
+  description: "나누면 배가 되고",
+  author: "Hyeonic",
+  siteUrl: "https://hyeonic.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/hyeonic",
+    linkedIn: "https://www.linkedin.com/in/%EA%B8%B0%ED%98%84-%EC%B5%9C-1240aa225/",
+    // facebook: "https://www.facebook.com",
+    // twitter: "https://twitter.com",
+    // x: "https://x.com",
+    // instagram: "https://www.instagram.com",
+    // medium: "https://medium.com",
+    // blogger: "https://blogger.com",
+    // email: "mailto:dev.hyeonic@gmail.com",
+    // resume: "https://hyeonic.github.io",
+    link: "https://hyeonic.github.io"
+  },
+  utterances: {
+    repo: "hyeonic/blog-comment",
+    type: "pathname"
+  },
+}
